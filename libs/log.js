737812a5516f0d5264a43c0e8094cfea2b7f5850
Chesheersky <d.f.goryunov@gmail.com>
2015-09-12 18:03:45 +0200

    logs folders

--- libs/log.js
+++ libs/log.js
@@ -6,19 +6,19 @@ function getLogger(module) {
     return new winston.Logger({
         transports : [
           new winston.transports.File({
-            name:       'logs\info-file',
+            name:       'logs\\info-file',
             filename:   'filelog-info.log',
             level:      'info',
             label:      label
           }),
           new winston.transports.File({
-            name:       'logs\debug-file',
+            name:       'logs\\debug-file',
             filename:   'filelog-debug.log',
             level:      'debug',
             label:      label
           }),
           new winston.transports.File({
-            name:       'logs\error-file',
+            name:       'logs\\error-file',
             filename:   'filelog-error.log',
             level:      'error',
             label:      label
