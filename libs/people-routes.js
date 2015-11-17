4fae7202e864b58e906bda2040d6881184dcb532
Chesheersky <d.f.goryunov@gmail.com>
2015-11-17 21:43:41 +0100

    return person with attempts

--- libs/people-routes.js
+++ libs/people-routes.js
@@ -1,114 +1,142 @@
 //todo move to controllers
 
-var express         = require('express');
-var log             = require('./log')(module);
-var PeopleModel     = require('./mongoose').PeopleModel;
-var router          = express.Router();
+var express = require('express');
+var log = require('./log')(module);
+var PeopleModel = require('./mongoose').PeopleModel;
+var QuestsModel = require('./mongoose').QuestsModel;
+var router = express.Router();
 
 // middleware specific to this router
 router.use(function timeLog(req, res, next) {
-  log.info('Time: ', Date.now());
-  next();
-})
+    log.info('Time: ', Date.now());
+    next();
+});
 
-router.get('/people', function(req, res) {
+router.get('/people', function (req, res) {
     return PeopleModel.find(function (err, people) {
         if (!err) {
             return res.send(people);
         } else {
             res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({error: 'Server error'});
         }
     });
 });
 
-router.post('/people', function(req, res) {
+router.post('/people', function (req, res) {
     var person = new PeopleModel({
         name: req.body.name,
         phone: req.body.phone,
         description: req.body.description,
+        attempts: req.body.attempts,
         image: req.body.image
     });
 
     person.save(function (err) {
         if (!err) {
             log.info("person created");
-            return res.send({ status: 'OK', person:person });
+            return res.send({status: 'OK', person: person});
         } else {
             log.error(err);
-            if(err.name == 'ValidationError') {
+            if (err.name == 'ValidationError') {
                 res.statusCode = 400;
-                res.send({ error: 'Validation error' });
+                res.send({error: 'Validation error'});
             } else {
                 res.statusCode = 500;
-                res.send({ error: 'Server error' });
+                res.send({error: 'Server error'});
             }
             log.error(`Internal error(${res.statusCode}): ${err.message}`);
         }
     });
 });
 
-router.get('/people/:id', function(req, res) {
+router.get('/people/:id', function (req, res) {
     return PeopleModel.findById(req.params.id, function (err, person) {
-        if(!person) {
+        if (!person) {
             res.statusCode = 404;
-            return res.send({ error: 'Not found' });
+            return res.send({error: 'Not found'});
         }
         if (!err) {
-            return res.send({ status: 'OK', person:person });
+            return QuestsModel.find(function (err, quests) {
+                if (!err) {
+                    var copy = JSON.parse(JSON.stringify(person));
+                    quests.forEach(function (quest) {
+                        var attempt = copy.attempts.find(attempt=> {
+                            attempt.quest == quest._id
+                        });
+                        if (!attempt) {
+                            var newAttempt = {
+                                quest: quest,
+                                success: false,
+                                time: "0"
+                            };
+                            copy.attempts.push(newAttempt);
+                        }
+                        else {
+                            copy.attempts.push(attempt);
+                        }
+                    });
+                    return res.send({status: 'OK', person: copy});
+                } else {
+                    res.statusCode = 500;
+                    log.error('Internal error(%d): %s', res.statusCode, err.message);
+                    return res.send({error: 'Server error'});
+                }
+            });
         } else {
             res.statusCode = 500;
-            log.error('Internal error(%d): %s',res.statusCode,err.message);
-            return res.send({ error: 'Server error' });
+            log.error('Internal error(%d): %s', res.statusCode, err.message);
+            return res.send({error: 'Server error'});
         }
     });
 });
 
-router.put('/people/:id', function (req, res){
+router.put('/people/:id', function (req, res) {
     return PeopleModel.findById(req.params.id, function (err, person) {
-        if(!person) {
+        if (!person) {
             res.statusCode = 404;
-            return res.send({ error: 'Not found' });
+            return res.send({error: 'Not found'});
         }
 
         person.name = req.body.name;
         person.phone = req.body.phone;
         person.description = req.body.description;
         person.image = req.body.image;
+        person.attempts = req.body.attempts;
 
         return person.save(function (err) {
             if (!err) {
                 log.info("person updated");
-                return res.send({ status: 'OK', person:person });
+                return res.send({status: 'OK', person: person});
             } else {
-                if(err.name == 'ValidationError') {
+                if (err.name == 'ValidationError') {
                     res.statusCode = 400;
-                    res.send({ error: 'Validation error' });
+                    res.send({error: 'Validation error'});
                 } else {
                     res.statusCode = 500;
-                    res.send({ error: 'Server error' });
+                    res.send({error: 'Server error'});
                 }
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
             }
         });
     });
 });
 
-router.delete('/people/:id', function (req, res){
+router.delete('/people/:id', function (req, res) {
     return PeopleModel.findById(req.params.id, function (err, person) {
-        if(!person) {
+        if (!person) {
             res.statusCode = 404;
-            return res.send({ error: 'Not found' });
+            return res.send({error: 'Not found'});
         }
         return person.remove(function (err) {
             if (!err) {
                 log.info("person removed");
-                return res.send({ status: 'OK' });
+                return res.send({status: 'OK'});
             } else {
                 res.statusCode = 500;
-                log.error('Internal error(%d): %s',res.statusCode,err.message);
-                return res.send({ error: 'Server error' });
+                log.error('Internal error(%d): %s', res.statusCode, err.message);
+                return res.send({error: 'Server error'});
             }
         });
     });
